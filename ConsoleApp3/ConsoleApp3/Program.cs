f16448b6974be530496f89d62120c838b92515c0
David Motúz <David Motz@LAPTOP-694EBULK>
2018-09-18 09:23:46 +0200

    zadani classy lod

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lod boat1 = new Lod();
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/lod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Lod
+    {
+        private int locY = 5;
+        private int locX = 5;
+        private int lenght = 3;
+        private bool direction = true; // true = horizontální, false = vertikální
+
+        public void changeDirection(bool newDirection)
+        {
+            if (direction == true)
+            {
+                direction = false;
+            } else
+            {
+                direction = true;
+            }
+            
+        }
+    }
+}
